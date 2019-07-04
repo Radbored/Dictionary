c09882122d5647f33ca8c8ce2a053c455cb7aada
Radbored <52531107+Radbored@users.noreply.github.com>
2019-07-04 15:37:29 +0300

    Add files via upload

---
+++ model/ValidChecker.java
@@ -0,0 +1,50 @@
+public class ValidChecker implements Checker {
+
+    private String result;
+    private Dictionary dictionary;
+
+    public ValidChecker(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String resultForRemove(String key, String value) {
+        return result = key + ":" + value + " удален";
+    }
+
+    public String resultForGet(String key, String value) {
+        return result = key + ":" + value;
+    }
+
+    public String resultForPut(String key, String value) {
+        return result = key + ":" + value + " добавлен";
+    }
+
+    public boolean isValidKey(String key) {
+        if (key.length() < dictionary.getKeyLength()) {
+            result = "Ключ слишком короткий";
+            return false;
+        }
+
+        if (key.length() > dictionary.getKeyLength()) {
+            result = "Ключ слишком длинный";
+            return false;
+        }
+        if (!key.matches(dictionary.getKeyRegex())) {
+            result = "Не соответствует длинне";
+            return false;
+        }
+        return true;
+    }
+
+    public boolean keyContains(String key) {
+        if (!dictionary.getDictionary().containsKey(key)) {
+            result = "Не содержит ключ";
+            return false;
+        }
+        return true;
+    }
+}
