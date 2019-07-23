4a7f42a14e5d3cd1256b087e346af2fb0a047cad
Radbored <52531107+Radbored@users.noreply.github.com>
2019-07-23 10:50:46 +0300

    Delete Checker.java

--- src/com/lib/Checker.java
+++
@@ -1,9 +0,0 @@
-package com.lib;
-public interface Checker {
-    String getResult();
-    String resultForRemove(String key, String value);
-    String resultForGet(String key, String value);
-    String resultForPut(String key, String value);
-    boolean isValidKey(String key);
-    boolean keyContains(String key);
-}
